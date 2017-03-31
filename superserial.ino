2093c6b559b0e5504476b9198af9530cada0a026
NuclearManD <dylanbrophy@gmail.com>
2017-03-30 18:00:09 -0700

    idk

--- superserial.ino
+++ superserial.ino
@@ -2,7 +2,8 @@
 #define RX 6
 #define TX 9
 // wrt is defined for the test transmitter
-#define wrt
+//#define wrt
+byte WAIT=20;
 void setup() {
   // put your setup code here, to run once:
   pinMode(RX,INPUT);
@@ -12,13 +13,23 @@ void setup() {
   Serial.begin(19200);
   #endif
 }
-
+long valid=0;
+long total=0;
+long time=0;
 void loop() {
   // put your main code here, to run repeatedly:
   #ifndef wrt
-  Serial.write(wait_read());
+  if(wait_read()==0xAA)
+    valid+=100;
+  total++;
+  if(time+1000<millis()){
+    time=millis();
+    Serial.println(valid/total);
+  }
+  delay(1);
+  write(1);
   #else
-  println("Yay! this worked!");
+  write(0xAA);
   #endif
 }
 long ls=0;
@@ -26,11 +37,16 @@ void write(byte x){
   // Initiate transfer
   while(ls+40>micros());
   digitalWrite(TX,LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(WAIT*2);
   ls=micros();
   byte i=0;
   while(ls+500>micros()&&i<8){
-    while(digitalRead(RX));
+    while(digitalRead(RX)){
+      if(ls+500<micros()){
+        digitalWrite(TX,HIGH);
+        return;
+      }
+    }
     digitalWrite(TX, 1&(x>>i));
     while(!digitalRead(RX));
     i++;
@@ -47,9 +63,9 @@ byte wait_read(){
   // (indicated by a low RX)
   while(digitalRead(RX));
   for(byte i=0;i<8;i++){
-    delayMicroseconds(20);
+    delayMicroseconds(WAIT);
     digitalWrite(TX, LOW);
-    delayMicroseconds(20);
+    delayMicroseconds(WAIT);
     x|=(digitalRead(RX)&1)<<i;
     digitalWrite(TX, HIGH);
   }
